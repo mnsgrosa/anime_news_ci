3e8e9c79276705173d06288b8a2bb1b001b87c0d
Matheus Rosa <matheusnsampaio@gmail.com>
2025-12-24 23:50:03 -0300

    backup

--- src/airflow/dags/api/anime.py
+++ src/airflow/dags/api/anime.py
@@ -1,70 +1,69 @@
 import httpx
+from constants import BASE_URL
 
 
-class AnimeHandler:
-    def __init__(self):
-        self.base_url = "https://api.jikan.moe/v4"
+def get_today_season():
+    url = f"{BASE_URL}/seasons/now"
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()
+    data = response.json()
 
-    def get_today_season(self):
-        url = f"{self.base_url}/seasons/now"
-        with httpx.Client() as client:
-            response = client.get(url)
-            response.raise_for_status()
-        data = response.json()
+    data_dictionary = {
+        "mal_id": data.get("mal_id", []),
+        "url": data.get("url", []),
+        "titles": data.get("titles", []),
+        "episodes": data.get("episodes", []),
+        "airing": data.get("airing", []),
+        "duration": data.get("duration", []),
+        "studios": data.get("studios", []),
+        "genres": data.get("genres", []),
+        "themes": data.get("themes", []),
+        "synopsis": data.get("synopsis", []),
+    }
 
-        data_dictionary = {
-            "mal_id": data.get("mal_id", []),
-            "url": data.get("url", []),
-            "titles": data.get("titles", []),
-            "episodes": data.get("episodes", []),
-            "airing": data.get("airing", []),
-            "duration": data.get("duration", []),
-            "studios": data.get("studios", []),
-            "genres": data.get("genres", []),
-            "themes": data.get("themes", []),
-            "synopsis": data.get("synopsis", []),
-        }
+    return data_dictionary
 
-        return data_dictionary
 
-    def get_upcoming_season(self):
-        url = f"{self.base_url}/seasons/upcoming"
-        with httpx.Client() as client:
-            response = client.get(url)
-            response.raise_for_status()
-        data = response.json()
+def get_upcoming_season():
+    url = f"{BASE_URL}/seasons/upcoming"
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()
+    data = response.json()
 
-        data_dictionary = {
-            "mal_id": data.get("mal_id", []),
-            "titles": data.get("titles", []),
-            "episodes": data.get("episodes", []),
-            "airing": data.get("airing", []),
-            "duration": data.get("duration", []),
-            "studios": data.get("studios", []),
-            "genres": data.get("genres", []),
-            "themes": data.get("themes", []),
-            "synopsis": data.get("synopsis", []),
-        }
+    data_dictionary = {
+        "mal_id": data.get("mal_id", []),
+        "titles": data.get("titles", []),
+        "episodes": data.get("episodes", []),
+        "airing": data.get("airing", []),
+        "duration": data.get("duration", []),
+        "studios": data.get("studios", []),
+        "genres": data.get("genres", []),
+        "themes": data.get("themes", []),
+        "synopsis": data.get("synopsis", []),
+    }
 
-        return data_dictionary
+    return data_dictionary
 
-    def get_anime_by_id(self, anime_id: int):
-        url = f"{self.base_url}/anime/{anime_id}"
-        with httpx.Client() as client:
-            response = client.get(url)
-            response.raise_for_status()
-        data = response.json()
 
-        data_dictionary = {
-            "mal_id": data.get("mal_id", []),
-            "titles": data.get("titles", []),
-            "episodes": data.get("episodes", []),
-            "airing": data.get("airing", []),
-            "duration": data.get("duration", []),
-            "studios": data.get("studios", []),
-            "genres": data.get("genres", []),
-            "themes": data.get("themes", []),
-            "synopsis": data.get("synopsis", []),
-        }
+def get_anime_by_id(anime_id: int):
+    url = f"{BASE_URL}/anime/{anime_id}"
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()
+    data = response.json()
 
-        return data_dictionary
+    data_dictionary = {
+        "mal_id": data.get("mal_id", []),
+        "titles": data.get("titles", []),
+        "episodes": data.get("episodes", []),
+        "airing": data.get("airing", []),
+        "duration": data.get("duration", []),
+        "studios": data.get("studios", []),
+        "genres": data.get("genres", []),
+        "themes": data.get("themes", []),
+        "synopsis": data.get("synopsis", []),
+    }
+
+    return data_dictionary
--- src/airflow/dags/handler/chunker.py
+++ src/airflow/dags/handler/chunker.py
@@ -1,3 +1,10 @@
 import tiktoken
+from haystack.components.preprocessors import DocumentSplitter
 
-def prepare_synopsis(synopsis: str, genre: str, name: str) -> str:
+
+def prepare_synopsis(synopsis: str, genre: str, name: str, date: str) -> str:
+    text_to_split = name + " " + genre + " " + synopsis
+
+    splitter = DocumentSplitter(split_by="word", split_length=400, split_threshold=100)
+
+    return splitter.run(documents=[text_to_split])
